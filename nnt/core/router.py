316fffa7f12e5efd132537b6ebdf62e52eb57f37
wybosys <wybosys@gmail.com>
2020-03-24 21:20:14 +0800

    prj

--- nnt/core/router.py
+++ nnt/core/router.py
@@ -1,4 +1,5 @@
 from ..manager import config
+import sys
 
 
 class IRouter:
@@ -64,48 +65,67 @@ class ActionProto:
         self.expose = False
 
 
-AP_KEY = "__actionproto"
+# 基于python的包装器的特性，函数包装执行时类对象并没有声明，所以之能采用全局映射函数数据组到类名上
+_actions = {}
 
 
-def action(model, options=None, comment=None):
+def action(mdlclz, options=None, comment=None):
     """ 定义router需要的model对象 """
     def _(fun):
         ap = ActionProto()
-        ap.clazz = model
+        ap.clazz = mdlclz
         ap.comment = comment
+
+        # 判断action是否在当前环境下开放
+        pas = True
         if options:
             for e in options:
                 setattr(ap, e, True)
-        # 判断有效性
-        pas = False
-        if not pas and ap.debug and config.DEBUG:
-            pas = True
-        if not pas and ap.develop and config.DEVELOP:
-            pas = True
-        if not pas and ap.local and config.LOCAL:
-            pas = True
-        if not pas and ap.devops and config.DEVOPS:
-            pas = True
-        if not pas and ap.devopsdevelop and config.DEVOPS_DEVELOP:
-            pas = True
-        if not pas and ap.devopsrelease and config.DEVOPS_RELEASE:
-            pas = True
+
+            # options默认为空代表开放，其他情形检测环境参数
+            pas = debug in options or develop in options or local in options or devops in options or devopsdevelop in options or devopsrelease in options
+
+            # 检测环境
+            if pas:
+                pas = False
+                if not pas and ap.debug and config.DEBUG:
+                    pas = True
+                if not pas and ap.develop and config.DEVELOP:
+                    pas = True
+                if not pas and ap.local and config.LOCAL:
+                    pas = True
+                if not pas and ap.devops and config.DEVOPS:
+                    pas = True
+                if not pas and ap.devopsdevelop and config.DEVOPS_DEVELOP:
+                    pas = True
+                if not pas and ap.devopsrelease and config.DEVOPS_RELEASE:
+                    pas = True
+
         if pas:
-            if not hasattr(model, AP_KEY):
-                setattr(model, AP_KEY, {})
-            model[AP_KEY][fun.__name__] = ap
+            # 获得action对应的router类
+            clazznm = fun.__qualname__.partition('.')[0]
+            modunm = fun.__module__
+            clazzpath = modunm + '.' + clazznm
+            if clazzpath not in _actions:
+                _actions[clazzpath] = {}
+            _actions[clazzpath][fun.__name__] = ap
         return fun
     return _
 
 
 def FindAction(target, key: str) -> ActionProto:
-    if hasattr(target.__class__, AP_KEY):
-        return target.__class__[AP_KEY][key]
-    return None
+    clazz = target.__class__
+    modunm = clazz.__module__
+    clazzpath = modunm + '.' + clazz.__name__
+    if clazzpath not in _actions:
+        return None
+    return _actions[clazzpath][key]
 
 
-def GetAllActionNames(obj) -> [str]:
-    if hasattr(obj.__class__, AP_KEY):
-        aps = obj.__class__[AP_KEY]
-        return list(aps.keys())
-    return None
+def GetAllActionNames(target) -> [str]:
+    clazz = target.__class__
+    modunm = clazz.__module__
+    clazzpath = modunm + '.' + clazz.__name__
+    if clazzpath not in _actions:
+        return None
+    return list(_actions[clazzpath].keys())
