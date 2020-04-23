9def5b7cfdb0726d3902acf714964857433f98d9
wybosys <wybosys@gmail.com>
2020-04-23 17:04:30 +0800

    添加依赖库安装脚本

---
+++ install-deps.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+apt install python3-termcolor python3-redis python3-uvloop python3-magic python3-flask python3-flask-compress python3-psutil python3-pyparsing python3-opencv python3-gevent python3-twisted python3-apscheduler python3-sqlalchemy python3-mysqldb
+pip3 install leveldb japronto ashes
+
+
